4ed2736678986da7a238a3a41eabb29012e223eb
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-12-08 21:38:46 +0530

    Connect button added

---
+++ frontend/app/MainProvider.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React from "react";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  Locale,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { optimismGoerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    optimismGoerli,
+
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [optimismGoerli]
+      : []),
+  ],
+  [publicProvider()]
+);
+
+const projectId = process.env.NEXT_PUBLIC_RAINBOW_PROJECT_ID || "none";
+
+const { wallets } = getDefaultWallets({
+  appName: "Prize Loans - PoolTogether x SuperFluid",
+  projectId,
+  chains,
+});
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+const demoAppInfo = {
+  appName: "Prize Loans - PoolTogether x SuperFluid",
+};
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={demoAppInfo}>
+        {mounted && children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
